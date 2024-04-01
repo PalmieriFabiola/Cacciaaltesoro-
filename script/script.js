ddab87cbe3760d9b30f7b45099c1c725ac52ac75
PalmieriFabiola <162125548+PalmieriFabiola@users.noreply.github.com>
2024-04-01 13:28:06 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 function doLogin()  {
-   document.getElementById("username").value;
-   document.getElementById("password").value;
-   if(document.getElementById("username").value == " " || document.getElementById("password").value == " ") {
+   var username= document.getElementById("username").value;
+   var password= document.getElementById("password").value;
+   if( username == " " || password == " ") {
      alert("Username e password sono obbligatori");
    }
 }
