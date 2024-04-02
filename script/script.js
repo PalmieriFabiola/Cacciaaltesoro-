26b63c46b8d1ad9e2a16f440c15b1911b32dcee8
PalmieriFabiola <162125548+PalmieriFabiola@users.noreply.github.com>
2024-04-02 11:12:08 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-function doLogin()  {
+function doLogin(event)  {
    var username= document.getElementById("username").value;
    var password= document.getElementById("password").value;
    if( username == " " || password == " ") {
