180e29972ba47bb2552d11a2ce031058ee9ce115
PalmieriFabiola <162125548+PalmieriFabiola@users.noreply.github.com>
2024-05-11 22:09:27 +0200

    Update script2.js

--- script/script2.js
+++ script/script2.js
@@ -1,1 +1,17 @@
-
+else {
+fetch('https://www.venditeOnline.org/login.php/loginIpad',{ //nome indirizzo server che si occupa di trattenere i dati riguardo le vendite di dispositivi
+   method:'POST',
+   headers:{
+      'Content-Type'= 'application/json',
+   }
+   body: JSON.stringify({
+    "username": username,
+   "password" : password
+})
+})
+.then(response=>response.json())
+.then(data=>console.log(data))
+.catch((error) => {
+   console.error('Errore:', error),
+})
+}
