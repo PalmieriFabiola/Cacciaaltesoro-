1d197b22fb58b44af5b04a3723e82829f5d03dad
PalmieriFabiola <162125548+PalmieriFabiola@users.noreply.github.com>
2024-05-12 10:35:56 +0200

    Update script2.js

--- script/script2.js
+++ script/script2.js
@@ -15,20 +15,27 @@ fetch('https://www.venditeOnline.org/login.php/loginIpad',{ //nome indirizzo ser
    { "modello": "Huawei P60 Pro"
       "prezzo": 999
       "fotocamera": "Fotocamera ultragrandangolare" 
+      "processore": "Snapdragon 8+ Gen 1 4G"
+      "batteria": 4815 mAh
    },
    { "modello": "Huawei P30 Lite"
       "prezzo": 136
       "fotocamera": "Tripla fotocamera"
+      "processore": "Huawei HiSilicon Kirin 710"
+      "batteria": 3340 mAh
    },
    { "modello": "Huawei P20"
       "prezzo": 200
       "fotocamera": "Doppia fotocamera"
+      "processore": "Kirin 970"
+      "batteria": 3320 mAh
    },
    { "modello": "Huawei P Smart"
       "prezzo": 110
       "fotocamera": "doppia fotocamera"
+      "processore": "Hisilicon Kirin 659"
+      "batteria": 3400 mAh
    }
-
 ]
 
 
