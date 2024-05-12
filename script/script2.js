64bf3b4b76e80230802bfe02725923540889d35d
PalmieriFabiola <162125548+PalmieriFabiola@users.noreply.github.com>
2024-05-12 10:21:32 +0200

    Update script2.js

--- script/script2.js
+++ script/script2.js
@@ -4,10 +4,36 @@ fetch('https://www.venditeOnline.org/login.php/loginIpad',{ //nome indirizzo ser
    headers:{
       'Content-Type'= 'application/json',
    }
-   body: JSON.stringify({
-    "username": username,
-   "password" : password
-})
+   body: JSON.stringify(
+   { 
+     "modello": "Galaxy s24",  // singolo oggetto 
+     "prezzo" : 629.00
+     "fotocamera":"camera tripla"
+   }
+   
+[                                     // lista di oggetti di dispositivi Huawei
+   { "modello": "Huawei P60 Pro"
+      "prezzo": 999
+      "fotocamera": "Fotocamera ultragrandangolare" 
+   },
+   { "modello": "Huawei P30 Lite"
+      "prezzo": 136
+      "fotocamera": "Tripla fotocamera"
+   },
+   { "modello": "Huawei P20"
+      "prezzo": 200
+      "fotocamera": "Doppia fotocamera"
+   },
+   { "modello": "Huawei P Smart"
+      "prezzo": 110
+      "fotocamera": "doppia fotocamera"
+   }
+
+]
+
+
+
+)
 })
 .then(response=>response.json())
 .then(data=>console.log(data))
